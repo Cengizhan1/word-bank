1f65ad06a077be570548a032a6af073858f6f09b
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-05-29 14:26:58 +0300

    fixed

--- backend/src/main/java/com/word/example/backend/user/service/AuthenticationService.java
+++ backend/src/main/java/com/word/example/backend/user/service/AuthenticationService.java
@@ -1,9 +1,9 @@
 package com.word.example.backend.user.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.word.example.backend.user.dto.AuthenticationResponse;
-import com.word.example.backend.user.dto.LoginRequest;
-import com.word.example.backend.user.dto.RegisterRequest;
+import com.word.example.backend.user.dto.auth.AuthenticationResponse;
+import com.word.example.backend.user.dto.auth.LoginRequest;
+import com.word.example.backend.user.dto.auth.RegisterRequest;
 import com.word.example.backend.user.exception.UsernameAlreadyExistsException;
 import com.word.example.backend.user.model.Token;
 import com.word.example.backend.user.model.User;
@@ -56,6 +56,7 @@ public class AuthenticationService {
                 .surname(request.surname())
                 .username(request.username())
                 .email(request.email())
+                .phone(request.phone())
                 .password(passwordEncoder.encode(request.password()))
                 .role(Role.ROLE_USER)
                 .build();
